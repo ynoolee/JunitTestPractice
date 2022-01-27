a92c4b4718df2e2154d748dfda6dd440fef96cef
ynoolee <53856184+ynoolee@users.noreply.github.com>
2022-01-27 21:02:22 +0900

    comment: context caching

--- src/test/java/com/example/test/test/SameApplicationContextTestA.java
+++ src/test/java/com/example/test/test/SameApplicationContextTestA.java
@@ -15,6 +15,7 @@ public class SameApplicationContextTestA {
 
     @Test
     public void isSame(){
+        // SameApplicationContextTestB 에서 출력되는 참조값과 동일 ( 테스트가 같은 프로세스에서 실행 될 때)
         System.out.println("SameApplicationContextTestA applicationContext = " + applicationContext);
     }
 }
--- src/test/java/com/example/test/test/SameApplicationContextTestB.java
+++ src/test/java/com/example/test/test/SameApplicationContextTestB.java
@@ -18,6 +18,7 @@ public class SameApplicationContextTestB {
 
     @Test
     public void isSame(){
+        // SameApplicationContextTestA 에서 출력되는 참조값과 동일 ( 테스트가 같은 프로세스에서 실행 될 때)
         System.out.println("SameApplicationContextTestB applicationContext = " + applicationContext);
     }
 }
