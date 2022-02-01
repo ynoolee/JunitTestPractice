02ddc6fd87b4d96d9c34f1b37c06b56482aa306b
ynoolee <53856184+ynoolee@users.noreply.github.com>
2022-02-01 20:51:41 +0900

    refactor: java 학습 패키지 추가에 따라 패키지 추가 및 변경

--- src/main/java/com/example/test/test/config/SameAppConfig.java
+++ src/main/java/com/example/test/test/config/SameAppConfig.java
@@ -1,10 +1,8 @@
-package com.example.test.test;
+package com.example.test.test.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SameAppConfig {
-
-
 }
