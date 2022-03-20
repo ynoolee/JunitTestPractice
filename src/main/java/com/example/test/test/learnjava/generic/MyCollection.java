6d017344960eb972e08496c3b7c0a283c97e67f6
ynoolee <53856184+ynoolee@users.noreply.github.com>
2022-03-20 16:20:38 +0900

    add : generic collection 추가 ( 커밋을 깜빡했었음 )

---
+++ src/main/java/com/example/test/test/learnjava/generic/MyCollection.java
@@ -0,0 +1,13 @@
+package com.example.test.test.learnjava.generic;
+
+public class MyCollection<T> {
+    T data;
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
