bc13039cabe8af5f2f05048f582b4ece1e4f3d2e
ynoolee <heerarea129@gmail.com>
2022-03-28 21:13:52 +0900

    Add : Primitive 를 위한 람다를 사용해야하는 이유 테스트
    - 시나리오 : int 형 배열 두개를 생성하여, index 가 i 인 값을 더해 새로운 int 형 배열에 할당한다
    - 당연하지만 제네릭 타입에  Primitive type 을 사용할 수 없다.
    - BiFunction 을 사용할 경우, 내부적으로 박싱 언박싱이 일어나고 있다. 이로 인해 성능 저하가 발생한다고 한다.
    - 이를 테스트하기 위해, (multiple parameter 를 받을 수 있는 ) Primitive 를 위한 함수형 인터페이스 와 참조타입(여기선 Wrapper class 타입)을 사용해야하는 BiFunction 으로 같은 기능을 수행하도록 하였다.
    - 테스트 결과, Primitive 를 위한 함수형 인터페이스를 사용할 경우 훨씬 적은 시간이 걸렸다.

---
+++ src/test/java/com/example/test/test/learnjava/lambda/LambdaForPrimitiveTest.java
@@ -0,0 +1,47 @@
+package com.example.test.test.learnjava.lambda;
+
+import java.util.Random;
+import java.util.function.BiFunction;
+import java.util.function.IntBinaryOperator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LambdaForPrimitiveTest {
+	private final int LEN = 10000;
+	private int[] op1 = new int[LEN];
+	private int[] op2 = new int[LEN];
+	private int[] res = new int[LEN];
+
+	private IntBinaryOperator operator;
+	private BiFunction<Integer,Integer,Integer> wrapperOperator;
+
+	@BeforeEach
+	public void setUp(){
+		Random rand = new Random();
+
+		operator = (a,b) -> b + a;
+		wrapperOperator = (a,b) -> b+a;
+
+		for(int i = 0 ; i< LEN;i++){
+			op1[i] = rand.nextInt(200000);
+			op2[i] = rand.nextInt(200000);
+		}
+	}
+
+	@Test
+	public void primitive_타입을_위한_람다와_참조타입에대한_람다사용_비교(){
+		long start =  0L;
+		start = System.nanoTime();
+		for(int i =0; i < LEN; i++){
+			res[i] = operator.applyAsInt(op1[i], op2[i]);
+		}
+		System.out.println(System.nanoTime() - start);
+
+		start = System.nanoTime();
+		for(int i =0; i < LEN; i++){
+			res[i] = wrapperOperator.apply(op1[i],op2[i]);
+		}
+		System.out.println(System.nanoTime() - start);
+	}
+
+}
